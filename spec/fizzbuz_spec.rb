d88682e7dd7922f6ef0c25fd4696366fe39f741a
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-01-22 13:22:27 +0000

    add test that it returns fizzbuzz when passed number that is divisible by both 3 and 5

--- spec/fizzbuz_spec.rb
+++ spec/fizzbuz_spec.rb
@@ -11,8 +11,13 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(23)).to eq 23
   end
 
-  it 'returns buzz when passed a number that is divisible by 5' do
+  it 'returns buzz when passed a number that is divisible by 5 but not 3' do
     expect(fizzbuzz(5)).to eq 'buzz'
     expect(fizzbuzz(25)).to eq 'buzz'
+  end
+
+  it 'returns fizzbuzz when passed a number that is divisible by both 5 and 3' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(60)).to eq 'fizzbuzz'
   end
 end
