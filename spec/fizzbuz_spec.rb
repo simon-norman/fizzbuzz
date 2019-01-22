8a36f31356ebd398c6e17476d699ed271057ecea
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-01-22 13:07:01 +0000

    add test that it returns the number if not divisible by 3 or 5

--- spec/fizzbuz_spec.rb
+++ spec/fizzbuz_spec.rb
@@ -1,5 +1,13 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns the number when passed a number that is not divisible by 3 or 5' do
+    expect(fizzbuzz(4)).to eq 4
+    expect(fizzbuzz(7)).to eq 7
+    expect(fizzbuzz(23)).to eq 23
   end
 end
